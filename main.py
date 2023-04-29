a737306b2a44b16a18efc4bf2e9c2e69530a4206
Nikita Smykov <apic@yandex.ru>
2023-04-29 12:01:34 +0300

    Additional PEP8 refactoring

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ def attack(char_name, char_class):
 
 def defence(char_name, char_class):
     if char_class == 'warrior':
-        return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
+        return f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
-        return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
+        return f'{char_name} блокировал {10 + randint(-2, 2)} урона'
     if char_class == 'healer':
-        return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
+        return f'{char_name} блокировал {10 + randint(2, 5)} урона'
 
 
 def special(char_name, char_class):
@@ -27,9 +27,9 @@ def special(char_name, char_class):
         return (f'{char_name} применил специальное умение'
                 f'«Выносливость {80 + 25}»')
     if char_class == 'mage':
-        return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
+        return f'{char_name} применил специальное умение «Атака {5 + 40}»'
     if char_class == 'healer':
-        return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+        return f'{char_name} применил специальное умение «Защита {10 + 30}»'
 
 
 def start_training(char_name, char_class):
